2690f132e1808820907fc40d0b317f95b8f181b7
Bohdana Omelchuk <dytynchukbogdana@gmail.com>
2023-03-02 19:11:16 +0200

    ball colors

--- ball.js
+++ ball.js
@@ -5,7 +5,7 @@ let ctx = canvas.getContext("2d");
 let Ball = function () {
     this.x = 100;
     this.y = 100;
-    this.xSpeed = -2;
+    this.xSpeed = -3;
     this.ySpeed = 3;
 };
 
@@ -19,6 +19,14 @@ let circle = function (x, y, radius, fillCircle){
   }
 };
 
+let colors = ["Green", "Red", "Gold", "Lime", "MediumPurple", "Crimson", "Thistle", "YellowGreen",
+    "Navy", "Blue", "Coral", "Cyan", "DarkMagenta", "DarkGoldenRod", "DeepPink", "DodgerBlue",
+    "DarkSlateGrey", "Grey", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Purple", "Sienna",];
+function ballColor() {
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    return randomColor;
+}
+
 Ball.prototype.draw = function () {
     circle(this.x, this.y, 25, true);
 };
@@ -29,19 +37,29 @@ Ball.prototype.move = function () {
 Ball.prototype.checkCollision  = function () {
     if(this.x < 0 || this.x > 800) {
         this.xSpeed = -this.xSpeed;
+        ctx.fillStyle = ballColor();
     }
     if(this.y < 0 || this.y > 800) {
         this.ySpeed = -this.ySpeed;
+        ctx.fillStyle = ballColor();
     }
 };
 
-let ball = new Ball();
+let balls = [];
+for(let i = 0; i < 10; i++){
+    balls.push(new Ball());
+}
+
+console.log(balls);
+
+// let ball = new Ball();
 setInterval(function () {
     ctx.clearRect(0, 0,800,800);
-
-    ball.draw();
-    ball.move();
-    ball.checkCollision();
+for(let i = 0; i < balls.length; i++){
+    balls[i].draw();
+    balls[i].move();
+    balls[i].checkCollision();
+}
 
     ctx.strokeRect(0,0,800,800);
 }, 10);
